a3f4d95e45708fabb9c03e40ae3e5ee9a18fc185
Chae Jubb <cjubb39@gmail.com>
2015-04-01 20:12:10 -0400

    mydata proper include guard

--- svd/src/mydata.h
+++ svd/src/mydata.h
@@ -1,6 +1,11 @@
+#ifndef _MYDATA_H_
+#define _MYDATA_H_
+
 #define MAX_SIZE 25
 
 typedef struct svd_token {
 	double matrix[MAX_SIZE * MAX_SIZE]; /* row major form */
 	unsigned size; /* of one dimension */
 } svd_token_t;
+
+#endif
