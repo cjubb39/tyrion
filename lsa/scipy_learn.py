6bad9aad6cb84a998d2b8f269953ebbb14ab631d
Ruchir Khaitan <rk2660@columbia.edu>
2015-04-11 20:03:38 -0400

    testing lsa tutorial matrix

--- lsa/scipy_learn.py
+++ lsa/scipy_learn.py
@@ -1,11 +1,32 @@
-from numpy import matrix 
+from numpy import matrix, loadtxt, delete  
 from scipy.linalg import qr, svd
 
 
-if __name__ == "main":
-    filename = "tutorial-input.txt"
+if __name__ == "__main__":
+    filename = "tutorial_input.txt"
     with open(filename, "r") as f: 
-        text = f.read() 
-        a = matrix(text) 
+        text = f.read()
+        #print text 
+        a = loadtxt(filename) 
+        #print(a)
+        u, s, vh = svd(a)
+        #print ("Singular values")
+        #print(s)
 
-        print a
+        q, r = qr(a)
+        #print ("Q and R in A = QR")
+        #print (q)
+        #print (r)
+        r = delete(r,(5, 6, 7),axis=0)
+        #print (r)
+        #print ("SVD of R") 
+        uu, ss, vvh = svd(r) 
+
+        #print(ss)
+        matrix_str = ""
+        for row in r:
+            row_str = "\t".join(str(e) for e in row) 
+            row_str += "\n"
+            matrix_str += row_str
+        print len(r)
+        print matrix_str
