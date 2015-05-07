838eb0fa4b6809a22007e938acdad5a95c42f84f
Ruchir Khaitan <rk2660@columbia.edu>
2015-05-07 19:03:25 -0400

    eliminated the s matrix

--- svd/src/svd.cpp
+++ svd/src/svd.cpp
@@ -146,8 +146,23 @@ COPY_SINGULAR_VALUES:
 		SVD_CELL_TYPE tmp;
 		tmp = a[(i * n) + j];
 		wait();
-		s[i] = tmp;
+		a[i] = tmp;
+        if(i) {
+            a[(i * n) + j] = 0.0; 
+        }
 	}
+/*
+    // Copy over the singular values in a to s
+COPY_SINGULAR_VALUES:
+    for (i = 0; i < MAX_SIZE; i++) {
+        if (i == n) break;
+        j = i;
+        SVD_CELL_TYPE tmp;
+        tmp = a[(i * n) + j];
+        wait();
+        s[i] = tmp;
+    }
+*/ 
 
 	return;
 }
@@ -593,9 +608,10 @@ OUTPUT_S_OUTER:
 		for (int i = 0; i < MAX_SIZE; ++i) {
 			if (i == rows) break;
 OUTPUT_S_INNER:
+            //replaced s with matrix_in 
 			for (int j = 0; j < MAX_SIZE; ++j) {
 				if (j == rows) break;
-				SVD_CELL_TYPE cell = s[i * size + j];
+				SVD_CELL_TYPE cell = matrix_in[i * size + j];
 				bufdout.put(cell);
 #ifdef VERBOSE
 				cout << "DUT PUT: (index, length, i, j, val)" << index << " " << length
@@ -690,7 +706,7 @@ DEBAYER_WHILE:
 		while (input_done.read());
 		process_start.write(false);
 
-		jacobi(matrix_in, rows, s, u, v);
+		jacobi(matrix_in, rows, NULL, u, v);
 
 		// 4-phase handshake
 		process_done.write(true);
