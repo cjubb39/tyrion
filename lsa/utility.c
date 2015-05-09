28ae53d356dd2d2faa9c0dc9f2f930de362d2469
Ruchir Khaitan <rk2660@columbia.edu>
2015-05-09 17:24:10 -0400

    bit vector funtimes

---
+++ lsa/utility.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h> 
+
+
+#define OFFSET 1023
+#define MAX 8388608
+
+void double_to_bv(double d, uint64_t* fixed) { 
+    double sub = MAX; 
+    *fixed = 0; 
+    int i; 
+    printf ("Double: %f ", d); 
+    for (i = 63; i >= 0; i--) {
+        if (d > sub) {
+        //set the bit and substract the value from sub 
+            d -= sub;
+            *fixed |= ( 1UL << i); 
+        }
+        sub /= 2.0; 
+    }
+
+    printf("BV: %lu\n", *fixed); 
+
+}
+
+void bv_to_double(uint64_t fixed, double* d) { 
+    double sub = MAX; 
+    *d = 0.0; 
+    int i; 
+
+    for (i = 63; i >= 0; i--) {
+        if ((fixed & (1UL << i))) {
+            
+            *d += sub;
+        }
+        sub /= 2.0; 
+    }
+
+    printf("BV: %lu  Double: %f \n", fixed, *d); 
+}
+
+
+
+int main(int argc, char** argv) {
+    srand(time(NULL)); 
+
+    int i;
+
+    uint64_t res; 
+    double random; 
+    double resd; 
+    double_to_bv((double) 0.0, &res);
+    bv_to_double(res, &resd);
+    double_to_bv((double) 0.25, &res); 
+    bv_to_double(res, &resd);
+    for (i = 0; i < 10; i++) {
+        random = rand()  / 13000.0; 
+        double_to_bv(random, &res); 
+        bv_to_double(res, &resd);
+    }
+}
+
+
